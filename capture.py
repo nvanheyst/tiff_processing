75afb9f3b4b92205b48044436f77ccb6cf9103da
Nathan Van Heyst <57072440+nvanheyst@users.noreply.github.com>
2024-09-10 09:53:16 -0400

    Create capture.py

---
+++ capture.py
@@ -0,0 +1,26 @@
+#capture.py
+
+import cv2
+cap = cv2.VideoCapture(“/dev/video0”)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 512)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('Y','1','6',' '))
+cap.set(cv2.CAP_PROP_CONVERT_RGB, 0)
+
+framecount=600
+frame_buf=[]
+for _ in range(framecount):#record indefinitely (until user presses q), replace with "while True"
+    stream_ret, frame = cap.read()
+    if stream_ret:
+        cv2.imshow("image", frame)
+        if cv2.waitKey(1) == ord('q'):
+            break;
+        frame_buf.append(frame)
+        
+cv2.destroyAllWindows()
+
+#Below is example code to save images to "folder" (Use appropriate directory syntax.)
+num=0
+while len(frame_buf)>0:
+    cv2.imwrite(f'{folder}/cap_{num}.tiff', frame_buf.pop(0))
+    num += 1
